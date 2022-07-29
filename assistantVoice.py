d9b764e0c00079c013377001c440ded9dd4cdd29
Ioanni Tsaka <iwanhstsakas2002@yahoo.com>
2022-07-29 03:24:41 +0300

    Version 1.0

--- assistantVoice.py
+++ assistantVoice.py
@@ -3,6 +3,8 @@ import speech_recognition
 import speech_recognition as sr
 import pyttsx3
 import sqlite3
+import pygame
+from tkinter import *
 
 WAKE = "hello assistant"
 
@@ -53,8 +55,41 @@ while a:
                 stripString2 = str(stripString1).rstrip(",")
                 stripString3 = str(stripString2).lstrip('"').rstrip('"')
                 stripString4 = str(stripString3).lstrip("'").rstrip("'")
-                os.startfile(str(stripString4))
-            speak("Opening" + text)
+                try:
+                    os.startfile(str(stripString4))
+                except OSError as e:
+                    root = Tk()
+                    root.attributes('-topmost',
+                                    True)  # Make error appear on top of screen, so it gets the user's attention
+                    try:
+                        photo = PhotoImage(file="backgrounds/error.ico")
+                        root.iconphoto(False, photo)
+                    except TclError:
+                        pass  # Uses the default logo tkinter provides, in case user deletes `error.ico`.
+
+                    root.title("ERROR")
+                    app_width = 430
+                    app_height = 70
+                    screen_width = root.winfo_screenwidth()
+                    screen_height = root.winfo_screenheight()
+                    x = (screen_width / 2) - (app_width / 2)
+                    y = (screen_height / 2) - (app_height / 2)
+                    root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
+                    root.resizable(False, False)
+                    pygame.mixer.init()
+                    error_text = Label(root,
+                                       text=text.capitalize() + " is either corrupted or an older version than the ones Windows supports.\n"
+                                                                "Deleting this app from the voice assistant is recommended. ")
+
+                    error_text.pack()
+
+                    ok_button = Button(root, text="     OK     ", command=root.destroy, bg='grey')
+                    ok_button.place(x=195, y=40)
+                    pygame.mixer.music.load('errorSound.mp3')
+                    pygame.mixer.music.play(loops=0)
+                    root.mainloop()
+                    break
+                speak("Opening" + text)
         elif not text:
             pass
         else:
